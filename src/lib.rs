2026d6e37fe2f06a489535a94711b2d495f84e2e
paulsohn <paulsohn@outlook.kr>
2023-08-09 23:14:43 +0900

    chap2-memmap

---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+#![no_std] 
+
+// copy implementation from the answer of:
+// https://stackoverflow.com/questions/50200268/how-can-i-use-the-format-macro-in-a-no-std-environment
+
+use core::fmt;
+
+pub struct BufferWriter<const CAP: usize> {
+    buf: [u8; CAP],
+    cursor: usize,
+}
+
+impl<const CAP: usize> BufferWriter<CAP> {
+    #[inline]
+    pub fn new() -> Self {
+        Self { buf: [0u8; CAP], cursor: 0 }
+    }
+
+    #[inline]
+    pub fn capacity(&self) -> usize {
+        self.buf.len()
+    }
+
+    #[inline]
+    pub fn clear(&mut self){
+        self.cursor = 0;
+    }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.cursor
+    }
+
+    #[inline]
+    pub fn empty(&self) -> bool {
+        self.cursor == 0
+    }
+
+    pub fn as_slice(&self) -> &[u8] {
+        &self.buf[..self.cursor]
+    }
+}
+
+impl<const CAP: usize> fmt::Write for BufferWriter<CAP> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        let cap = self.capacity();
+        for (i, &b) in self.buf[self.cursor..cap].iter_mut()
+            .zip(s.as_bytes().iter())
+        {
+            *i = b;
+        }
+        self.cursor = usize::min(cap, self.cursor + s.as_bytes().len());
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+// #![feature(asm)]
+// #![feature(abi_efiapi)]
+
+extern crate uefi;
+extern crate uefi_services;
+
+use uefi::prelude::*;
+// use uefi::CString16;
+use uefi_macros::cstr16;
+
+use core::fmt::Write;
+use mikanos_rust::BufferWriter;
+
+#[inline]
+fn uefi_boot(image_handle: uefi::Handle, mut system_table: SystemTable<Boot>) -> uefi::Result {
+    uefi_services::init(&mut system_table)?;
+
+    let stdout = system_table.stdout();
+    stdout.output_string(cstr16!("Hello, rust!\n"))?;
+
+    let boot_services = system_table.boot_services();
+    let mut mmap_buffer = [0u8; 0x4000]; // 16KiB
+    let mmap = boot_services.memory_map(&mut mmap_buffer)?; // now can iterate mmap.entries()
+
+    let mut root_dir = boot_services.get_image_file_system(image_handle)?;
+
+    // we want to open a file, grab the handle and append its contents as many time as we want
+    // unfortunately, current `uefi` crate encapsulate all process and only allow us a one-time cascading write.
+
+    let mut content_buffer = BufferWriter::<0x1000>::new();
+
+    for (i, desc) in mmap.entries().enumerate() {
+        writeln!(content_buffer,
+            "{},{:X},{:?},{:08X},{:X},{:X}",
+            i, desc.ty.0, desc.ty, desc.phys_start, desc.page_count, desc.att.bits()
+        );
+    }
+    root_dir.write(cstr16!("memmap.csv"), content_buffer.as_slice())
+        .map_err(|err| {
+            if let uefi::fs::Error::Io(err) = err {
+                err.uefi_error
+            } else {
+                unreachable!("The error should be IO error.")
+            }
+        });
+
+    boot_services.stall(10_000_000);
+    // loop {};
+
+    Ok(())
+}
+
+#[entry]
+fn uefi_start(image_handle: uefi::Handle, mut system_table: SystemTable<Boot>) -> Status {
+    match uefi_boot(image_handle, system_table){
+        Ok(()) => Status::SUCCESS,
+        Err(err) => err.status(),
+    }
 }
