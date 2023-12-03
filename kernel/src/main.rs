7dd2afd24817f45e0621073398138a7076951141
Taeseung Sohn <paulsohn@outlook.kr>
2023-12-03 07:33:24 +0000

    logging in main

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -40,18 +40,57 @@ struct Listeners;
 impl SupportedClassListeners for Listeners {
     fn keyboard() -> fn(KeyboardReport) {
         fn keyboard_listener(report: KeyboardReport) {
-            console_println!("Keyboard Report) modifier : {}", report.modifier);
+            log::debug!("Keyboard Report) modifier : {}", report.modifier);
         }
 
         keyboard_listener
     }
     fn mouse() -> fn(MouseReport) {
         fn mouse_listener(report: MouseReport) {
-            console_println!("Mouse Report) {}, {}, {}", report.buttons, report.x, report.y);
+            log::debug!("Mouse Report) {}, {}, {}", report.buttons, report.x, report.y);
         }
 
         mouse_listener
     }
+}
+
+fn setup_xhc_controller() -> Option<Controller<Listeners>> {
+    use kernel::pci::Devices;
+    let devices = Devices::scan().unwrap();
+    for dev in devices.as_slice() {
+        log::debug!("{}.{}.{}.: vend {:04x}, class {:06x}, head {:02x}", dev.bus(), dev.slot_fun().0, dev.slot_fun().1, dev.vendor_id(), dev.class_code().code(), dev.header_type());
+    }
+
+    let mmio_base = devices.as_slice().iter().find(|&dev| {
+        dev.class_code().match_base_sub_interface(0x0c, 0x03, 0x30) && dev.vendor_id() == 0x8086
+    }).and_then(|xdev| {
+        log::debug!("An Intel xHC has been detected.");
+
+        let switch_ehci_to_xhci = devices.as_slice().iter().find(|&dev| {
+            dev.class_code().match_base_sub_interface(0x0c, 0x03, 0x20)
+        }).is_some();
+
+        if switch_ehci_to_xhci {
+            // read and write `xdev`
+            xdev.read_write_offset(0xdc, 0xd8); // Superspeed Ports
+            xdev.read_write_offset(0xd4, 0xd0); // eHCi to xHCi ports
+            log::debug!("Switched eHCi to xHCi.");
+        }
+
+        Some(xdev)
+    }).or_else(|| {
+        devices.as_slice().iter().find(|&dev| {
+            dev.class_code().match_base_sub_interface(0x0c, 0x03, 0x30)
+        })
+    }).and_then(|xdev| {
+        log::debug!("xHC has been found: {}.{}.{}.", xdev.bus(), xdev.slot_fun().0, xdev.slot_fun().1);
+
+        xdev.bar0().mm()
+    })?;
+
+    Some(
+        Controller::new(mmio_base, global_allocator())
+    )
 }
 
 #[no_mangle]
@@ -63,7 +102,7 @@ pub extern "sysv64" fn _start (
         frame_buffer_info
     );
 
-    console_println!("Hello, GYUR OS!");
+    log::info!("Hello, GYUR OS!");
 
     {
         let mut screen_cell = globals::SCREEN.lock();
@@ -85,49 +124,12 @@ pub extern "sysv64" fn _start (
         });
     }
 
-    use kernel::pci::Devices;
-    let devices = Devices::scan().unwrap();
-    for dev in devices.as_slice() {
-        console_println!("{}.{}.{}.: vend {:04x}, class {:06x}, head {:02x}", dev.bus(), dev.slot_fun().0, dev.slot_fun().1, dev.vendor_id(), dev.class_code().code(), dev.header_type());
-    }
+    let mut xhc = setup_xhc_controller().unwrap();
+    xhc.run();
+    xhc.reconfigure_port();
 
-    let opt_mmio_base = devices.as_slice().iter().find(|&dev| {
-        dev.class_code().match_base_sub_interface(0x0c, 0x03, 0x30) && dev.vendor_id() == 0x8086
-    }).and_then(|xdev| {
-        console_println!("An Intel xHC has been detected.");
-
-        let switch_ehci_to_xhci = devices.as_slice().iter().find(|&dev| {
-            dev.class_code().match_base_sub_interface(0x0c, 0x03, 0x20)
-        }).is_some();
-
-        if switch_ehci_to_xhci {
-            // read and write `xdev`
-            xdev.read_write_offset(0xdc, 0xd8); // Superspeed Ports
-            xdev.read_write_offset(0xd4, 0xd0); // eHCi to xHCi ports
-            console_println!("Switched eHCi to xHCi.");
-        }
-
-        Some(xdev)
-    }).or_else(|| {
-        devices.as_slice().iter().find(|&dev| {
-            dev.class_code().match_base_sub_interface(0x0c, 0x03, 0x30)
-        })
-    }).and_then(|xdev| {
-        console_println!("xHC has been found: {}.{}.{}.", xdev.bus(), xdev.slot_fun().0, xdev.slot_fun().1);
-
-        xdev.bar0().mm()
-    });
-
-    if let Some(mmio_base) = opt_mmio_base {
-        console_println!("BAR xHC MMIO base: {:08x}", mmio_base);
-        
-        let mut xhc: Controller<Listeners> = Controller::new(mmio_base, global_allocator());
-        xhc.run();
-        xhc.reconfigure_port();
-
-        loop {
-            xhc.process_events();
-        }
+    loop {
+        xhc.process_events();
     }
 
     halt();
